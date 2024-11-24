c7472384172a5dbf65a249654072da8e49d8e99e
Qinyao Lin <qinyaolin71@gmail.com>
2024-11-24 17:26:20 +0000

    populate partial complaint_fact_table with date_id

--- models/food_establishment_date_transform.sql
+++ models/food_establishment_date_transform.sql
@@ -1,9 +1,15 @@
 
 WITH foodraw AS(
 SELECT * 
-FROM cis9440gp.RawData.FoodEstablishment
-)
-
+FROM cis9440gp.RawDataset.FoodEstablishment
+),
+foodraw_adddateid AS(
 SELECT FORMAT_DATE('%Y%m%d', DATE(created_date)) AS date_id,
         *
 FROM foodraw
+ORDER BY date_id
+)
+SELECT unique_key, date_id
+FROM foodraw_adddateid f
+INNER JOIN cis9440gp.dbt_qlin.date_dimension d
+ON f.date_id = d.date_dim_id
