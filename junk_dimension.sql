055848d1be5ef4919d00c53f7894c2b30b5d533f
Qinyao Lin <qinyaolin71@gmail.com>
2024-11-25 04:02:45 +0000

    create junk_dimension without surrogate key

---
+++ junk_dimension.sql
@@ -0,0 +1,26 @@
+-- models/junk_dimension.sql
+
+{{ config(materialized='table') }}
+
+WITH location AS(
+    SELECT DISTINCT(location_type) AS food_establishment_type,
+    FROM `cis9440gp.RawDataset.FoodEstablishment`
+),
+location_change AS(
+    SELECT food_establishment_type,
+    CASE
+        WHEN food_establishment_type IN ('Cafeteria - College/University', 'Cafeteria - Private School', 'Cafeteria - Public School') THEN 'Educational Cafeteria'
+        WHEN food_establishment_type IN ('Food Cart Vendor', 'Mobile Food Vendor', 'Street Vendor', 'Street Fair Vendor') THEN 'Mobile Food Service'
+        WHEN food_establishment_type IN ('Restaurant', 'Restaurant/Bar/Deli/Bakery') THEN 'Restaurant Types'
+        WHEN food_establishment_type IN ('Catering Service', 'Catering Hall') THEN 'Catering Operations'
+        ELSE 'Other Types'
+    END AS detailed_category
+    FROM location
+),
+status_check AS(
+SELECT DISTINCT(status)AS resolution_status
+FROM `cis9440gp.RawDataset.FoodEstablishment`
+)
+SELECT detailed_category, resolution_status
+FROM location_change
+CROSS JOIN status_check
